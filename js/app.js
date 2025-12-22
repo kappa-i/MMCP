6d8c99a0bb70f32a3550e64d91074f0dce761a54
kappa-i <gaby.kappai@gmail.com>
2025-12-22 20:56:44 +0100

    .

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+function preventDefault(e) {
+  e.preventDefault();
+}
+
 function disableScroll() {
   document.body.style.overflow = 'hidden';
   document.addEventListener('touchmove', preventDefault, { passive: false });
@@ -8,10 +12,6 @@ function enableScroll() {
   document.body.style.overflow = '';
   document.removeEventListener('touchmove', preventDefault);
   document.removeEventListener('wheel', preventDefault);
-}
-
-function preventDefault(e) {
-  e.preventDefault();
 }
 
 
