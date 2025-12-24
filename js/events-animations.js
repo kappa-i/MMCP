f8e8e8b8bda6cd46abdaf237b07daedd987f52e6
kappa-i <gaby.kappai@gmail.com>
2025-12-24 15:55:54 +0100

    .

--- js/events-animations.js
+++ js/events-animations.js
@@ -8,6 +8,7 @@ const modalOverlay = modal.querySelector(".overlay");
 const boxes = gsap.utils.toArray(".boxes-container .box");
 const boxesContent = gsap.utils.toArray(".box-content");
 let boxIndex = undefined;
+let modalButton = null;
 
 boxesContent.forEach((box, i) => {
   box.addEventListener("click", () => {
@@ -15,6 +16,13 @@ boxesContent.forEach((box, i) => {
       const state = Flip.getState(box);
       boxes[boxIndex].appendChild(box);
       boxIndex = undefined;
+
+      // Retirer le bouton lors de la fermeture
+      if (modalButton) {
+        modalButton.remove();
+        modalButton = null;
+      }
+
       gsap.to([modal, modalOverlay], {
         autoAlpha: 0,
         ease: "power1.inOut",
@@ -31,6 +39,22 @@ boxesContent.forEach((box, i) => {
       const state = Flip.getState(box);
       modalContent.appendChild(box);
       boxIndex = i;
+
+      // Créer et ajouter le bouton "En voir plus"
+      const eventPage = box.dataset.eventPage;
+      if (eventPage && !modalButton) {
+        modalButton = document.createElement('a');
+        modalButton.href = eventPage;
+        modalButton.className = 'modal-event-btn';
+        modalButton.textContent = 'En voir plus';
+        modalContent.appendChild(modalButton);
+
+        // Empêcher le bouton de fermer la modal
+        modalButton.addEventListener('click', (e) => {
+          e.stopPropagation();
+        });
+      }
+
       gsap.set(modal, { autoAlpha: 1 });
       Flip.from(state, {
         duration: 0.7,
